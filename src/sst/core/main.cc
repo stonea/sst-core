b9a53833abbbbcedb6055b93678e339168192cde
Andy Stone <stonea@users.noreply.github.com>
2025-08-26 12:33:21 -0500

    adds runstage timer
    which will be displayed when SST is run with --print-timing-info
    
    ---
    Signed-off-by: Andy Stone <stonea@users.noreply.github.com>

--- src/sst/core/main.cc
+++ src/sst/core/main.cc
@@ -420,6 +420,7 @@ struct SimThreadInfo_t
     // Time / stats information
     double      build_time;
     double      run_time;
+    double      run_stage_time;
     UnitAlgebra simulated_time;
     uint64_t    max_tv_depth;
     uint64_t    current_tv_depth;
@@ -608,7 +609,9 @@ start_simulation(uint32_t tid, SimThreadInfo_t& info, Core::ThreadSafe::Barrier&
 
     /* Run Simulation */
     if ( info.config->runMode() == SimulationRunMode::RUN || info.config->runMode() == SimulationRunMode::BOTH ) {
+        double clock_run_start = sst_get_cpu_time();
         sim->run();
+        info.run_stage_time = sst_get_cpu_time() - clock_run_start;
         barrier.wait();
 
         /* Adjust clocks at simulation end to
@@ -1154,6 +1157,7 @@ main(int argc, char* argv[])
     for ( uint32_t i = 1; i < world_size.thread; i++ ) {
         threadInfo[0].simulated_time = std::max(threadInfo[0].simulated_time, threadInfo[i].simulated_time);
         threadInfo[0].run_time       = std::max(threadInfo[0].run_time, threadInfo[i].run_time);
+        threadInfo[0].run_stage_time = std::max(threadInfo[0].run_stage_time, threadInfo[i].run_stage_time);
         threadInfo[0].build_time     = std::max(threadInfo[0].build_time, threadInfo[i].build_time);
 
         threadInfo[0].max_tv_depth = std::max(threadInfo[0].max_tv_depth, threadInfo[i].max_tv_depth);
@@ -1163,9 +1167,10 @@ main(int argc, char* argv[])
 
     double build_time = (end_serial_build - start) + threadInfo[0].build_time;
     double run_time   = threadInfo[0].run_time;
+    double run_stage_time = threadInfo[0].run_stage_time;
     double total_time = total_end_time - start;
 
-    double max_run_time = 0, max_build_time = 0, max_total_time = 0;
+    double max_run_time = 0, max_run_stage_time = 0, max_build_time = 0, max_total_time = 0;
 
     uint64_t local_max_tv_depth      = threadInfo[0].max_tv_depth;
     uint64_t global_max_tv_depth     = 0;
@@ -1182,6 +1187,7 @@ main(int argc, char* argv[])
     uint64_t local_sync_data_size = threadInfo[0].sync_data_size;
 
     MPI_Allreduce(&run_time, &max_run_time, 1, MPI_DOUBLE, MPI_MAX, MPI_COMM_WORLD);
+    MPI_Allreduce(&run_stage_time, &max_run_stage_time, 1, MPI_DOUBLE, MPI_MAX, MPI_COMM_WORLD);
     MPI_Allreduce(&build_time, &max_build_time, 1, MPI_DOUBLE, MPI_MAX, MPI_COMM_WORLD);
     MPI_Allreduce(&total_time, &max_total_time, 1, MPI_DOUBLE, MPI_MAX, MPI_COMM_WORLD);
     MPI_Allreduce(&local_max_tv_depth, &global_max_tv_depth, 1, MPI_UINT64_T, MPI_MAX, MPI_COMM_WORLD);
@@ -1194,6 +1200,7 @@ main(int argc, char* argv[])
 #else
     max_build_time            = build_time;
     max_run_time              = run_time;
+    max_run_stage_time        = run_stage_time;
     max_total_time            = total_time;
     global_max_tv_depth       = local_max_tv_depth;
     global_current_tv_depth   = local_current_tv_depth;
@@ -1237,6 +1244,7 @@ main(int argc, char* argv[])
         g_output.output("Simulation Timing Information (Wall Clock Times):\n");
         g_output.output("  Build time:                      %f seconds\n", max_build_time);
         g_output.output("  Run loop time:                   %f seconds\n", max_run_time);
+        g_output.output("    Run stage time:                %f seconds\n", max_run_stage_time);
         g_output.output("  Total time:                      %f seconds\n", max_total_time);
         g_output.output("\n");
         g_output.output(
